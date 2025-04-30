e2472bcea976c89c3e9fb98a408a2e81a91a1c21
cloud ninja ops <cloudninjaops@gmail.com>
2025-04-29 21:01:27 -0400

    test

--- ab.tf
+++ ab.tf
@@ -40,3 +40,48 @@ resource "aws_kms_key_policy" "update_policy_with_append" {
   key_id = data.aws_kms_key.golden_key.key_id
   policy = local.updated_policy
 }
+
+
+resource "null_resource" "append_kms_arn" {
+  provisioner "local-exec" {
+    command = <<EOT
+    set -e
+
+    ROLE_ARN="arn:aws:iam::${var.new_account_id}:role/test-tf-role"
+    POLICY_FILE="current_policy.json"
+
+    # Fetch current policy
+    aws kms get-key-policy \
+      --key-id "${var.kms_key_id}" \
+      --policy-name default \
+      --region "${var.region}" > ${POLICY_FILE}
+
+    # Append new role if not present (using jq)
+    jq --arg role "$ROLE_ARN" '
+      .Statement |= map(
+        if .Sid == "AllowExternalAccountUse" then
+          .Principal.AWS |= (
+            if type == "array" then
+              if index($role) == null then . + [$role] else . end
+            else
+              if . == $role then . else [$role, .] end
+            end
+          )
+        else .
+        end
+      )
+    ' ${POLICY_FILE} > updated_policy.json
+
+    # Apply updated policy
+    aws kms put-key-policy \
+      --key-id "${var.kms_key_id}" \
+      --policy-name default \
+      --policy file://updated_policy.json \
+      --region "${var.region}"
+    EOT
+  }
+
+  triggers = {
+    new_arn = var.new_account_id
+  }
+}
