dacc27f1d1fdde6d7755a11299534833497daf63
cloud ninja ops <cloudninjaops@gmail.com>
2025-06-09 13:15:05 -0400

    tf

--- ab.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  kendra_list = { for k, v in local.cloud_components.kendra : k => merge(
-    v,
-    {
-      s3_bucket_name = try(module.s3_bucket[v.s3_bucket_key].name, null)
-      s3_bucket_arn  = try(module.s3_bucket[v.s3_bucket_key].arn, null)
-    }
-  )}
-}
