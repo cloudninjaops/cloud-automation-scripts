a78fd3ca49844a770fb04acaddb173b1479aca95
cloud ninja ops <cloudninjaops@gmail.com>
2025-06-11 13:21:17 -0400

    tf

---
+++ gen-drv.tf
@@ -0,0 +1,58 @@
+locals {
+  vpc_endpoint_exists = try(contains(keys(local.cloud_components), "vpc_endpoints"), false)
+
+  vpc_endpoint_grouping = local.vpc_endpoint_exists ? {
+    for key, value in local.cloud_components.vpc_endpoints :
+    key => merge(value, { endpoint_key = key })
+  } : {}
+
+  vpc_endpoint_ordinals = flatten([
+    for key, value in local.vpc_endpoint_grouping : [
+      for idx in range(length([value])) : {
+        "${key}_${format("%03d", idx + 1)}" = merge(value, {
+          ordinal      = format("%03d", idx + 1),
+          endpoint_key = key
+        })
+      }
+    ]
+  ])
+
+  vpc_endpoint_final_list = merge([
+    for k, v in local.vpc_endpoint_ordinals : {
+      "${try(v.category, "default")}_${v.ordinal}" => v
+    }
+  ]...)
+
+  vpc_endpoint_name_map = {
+    for key, value in local.vpc_endpoint_final_list : value.endpoint_key => key
+  }
+}
+
+module "vpc_endpoint" {
+  source  = "app.terraform.io/fepoc/vpc-endpoint/aws"
+  version = "1.0.0"
+
+  for_each = local.vpc_endpoint_final_list
+
+  app_name       = local.app_name
+  env_name       = var.env_name
+  env_type       = var.env_type
+  region         = var.region
+  ordinal        = each.value.ordinal
+  service_type   = each.value.service_type # like "s3", "ec2", "dynamodb"
+  endpoint_type  = each.value.endpoint_type # "Interface", "Gateway", or "GatewayLoadBalancer"
+  private_dns_enabled = try(each.value.private_dns_enabled, true)
+
+  vpc_id         = data.aws_vpc.default.id
+  subnet_ids     = try(data.aws_subnets.all_vpc_subnets.ids, [])
+  route_table_ids = try(data.aws_route_tables.default.ids, [])
+
+  security_group_ids = try([
+    for sg in try(each.value.security_groups, []) :
+    module.security_group[sg].id
+  ], [])
+
+  gwlb_security_group_id = try(module.security_group[each.value.gwlb_sg].id, null)
+
+  tags = local.optional_tags
+}
