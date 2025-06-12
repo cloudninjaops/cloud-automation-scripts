771ebeb8d9a947f7f9a784d26cbd4fc38225c1e3
cloud ninja ops <cloudninjaops@gmail.com>
2025-06-12 16:25:57 -0400

    tf

--- ab.tf
+++ ab.tf
@@ -1,9 +1,23 @@
-locals {
-  referenced_security_groups = distinct(flatten([
-    for ep_key, ep_val in local.cloud_components.vpc_endpoints : 
-    try(ep_val.security_groups, [])
-  ]))
+resource "null_resource" "enable_private_dns" {
+  triggers = {
+    vpce_id = aws_vpc_endpoint.example.id
+  }
 
-  security_groups = try(local.cloud_components.security_groups, {})
+  provisioner "local-exec" {
+    command = <<EOT
+echo "Trying to enable private DNS for VPC Endpoint: ${aws_vpc_endpoint.example.id}"
+
+aws ec2 modify-vpc-endpoint \
+  --vpc-endpoint-id ${aws_vpc_endpoint.example.id} \
+  --private-dns-enabled
+
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Warning: Failed to enable Private DNS. It might already be enabled or unsupported for this service or VPC."
+else
+  echo "Private DNS enabled successfully."
+fi
+EOT
+  }
 }
-  policy = file("${path.module}/policies/s3_policy.json") # or inline string
