24c15d59dd3765a208b581a47179267ac2693951
cloud ninja ops <cloudninjaops@gmail.com>
2025-06-16 14:46:04 -0400

    tf

--- techstack.tf
+++ techstack.tf
@@ -13,3 +13,16 @@ EOF
 
 # ─── Add techstack_output.json to Git ──────────────────────────────────
 git add "$OUTPUT_JSON_PATH" || exit 1
+
+
+resource "local_file" "techstack_output" {
+  filename = "${path.module}/account_policy/techstack_output.json"
+
+  content = jsonencode({
+    techstack          = lower(var.techstack_name)
+    tfc_workspace_name = var.TFC_WORKSPACE_NAME
+    tfc_run_id         = var.TFC_RUN_ID
+  })
+
+  file_permission = "0644"
+}
