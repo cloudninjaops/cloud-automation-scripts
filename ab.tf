787f29f9b1a54ca03be30f5869928909ac7d8636
cloud ninja ops <cloudninjaops@gmail.com>
2025-07-07 13:54:28 -0400

    tf

--- ab.tf
+++ ab.tf
@@ -1,7 +1,7 @@
-resource "aws_iam_role" "cert_reader" {
-  count = var.cert_key_secret_name != null && var.acm_cert_arn != null ? 1 : 0
+resource "aws_iam_role" "cert_secret_reader" {
+  count = var.iam_instance_profile == null && var.cert_key_secret_name != null && var.acm_cert_arn != null ? 1 : 0
 
-  name = "${var.env_name}-${var.app_name}-cert-reader-role"
+  name = "${local.asg_cert_ec2_role_name_prefix}-cert-secret-reader-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -11,57 +11,31 @@ resource "aws_iam_role" "cert_reader" {
         Principal = {
           Service = "ec2.amazonaws.com"
         },
-        Effect = "Allow",
+        Effect = "Allow"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy" "cert_reader_policy" {
-  count = var.cert_key_secret_name != null && var.acm_cert_arn != null ? 1 : 0
-
-  name = "cert-access"
-  role = aws_iam_role.cert_reader[0].name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = ["secretsmanager:GetSecretValue"],
-        Resource = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:${var.cert_key_secret_name}*"
-      },
-      {
-        Effect   = "Allow",
-        Action   = ["acm:GetCertificate", "acm:DescribeCertificate"],
-        Resource = var.acm_cert_arn
-      }
-    ]
-  })
+=-----
+locals {
+  iam_instance_profile_name = (
+    var.iam_instance_profile != null ? var.iam_instance_profile :
+    (
+      var.cert_key_secret_name != null && var.acm_cert_arn != null ?
+      aws_iam_instance_profile.cert_secret_reader_profile[0].name :
+      null
+    )
+  )
 }
 
-resource "aws_iam_instance_profile" "cert_reader_profile" {
-  count = var.cert_key_secret_name != null && var.acm_cert_arn != null ? 1 : 0
+---
 
-  name = "${var.env_name}-${var.app_name}-cert-reader-profile"
-  role = aws_iam_role.cert_reader[0].name
-}
-
-
-#----
-
-resource "aws_launch_template" "this" {
-  ...
-
-  dynamic "iam_instance_profile" {
-    for_each = var.cert_key_secret_name != null && var.acm_cert_arn != null ? [1] : []
-    content {
-      name = aws_iam_instance_profile.cert_reader_profile[0].name
-    }
+dynamic "iam_instance_profile" {
+  for_each = local.iam_instance_profile_name != null ? [local.iam_instance_profile_name] : []
+  content {
+    name = iam_instance_profile.value
   }
-
-  ...
 }
 
-
-data "aws_caller_identity" "current" {}
+----
