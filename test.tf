3f0ed90287252a4dfd183068841e4d7ed8af8ff9
cloud ninja ops <cloudninjaops@gmail.com>
2025-10-15 13:59:40 -0400

    test hcl

--- test.tf
+++ test.tf
@@ -1,42 +1,20 @@
-# Existing data block for compliance user data
-data "template_cloudinit_config" "compliance" {
-  gzip          = true
-  base64_encode = true
-  # part {
-  #   filename     = "ssm.cfg"
-  #   content_type = "text/x-shellscript"
-  #   content      = templatefile("${path.module}/resources/user_data_scripts/ssm_config.sh", { AD = local.AD })
-  # }
-  # ... other parts (tenable.cfg, datadog.cfg, dynamic custom_user_data) ...
-}
+import "tfplan/v2" as tfplan
 
-# New data block to merge app_user_data with compliance data
-data "template_cloudinit_config" "merged" {
-  gzip          = true
-  base64_encode = true
+authenticated_users_uri = "http://acs.amazonaws.com/groups/global/AuthenticatedUsers"
 
-  # Include the original compliance data
-  part {
-    content_type = "text/cloud-config"
-    content      = base64decode(data.template_cloudinit_config.compliance.rendered)
-  }
+violating_acls = filter(tfplan.resource_changes or []) as address, rc {
+  rc.type is "aws_s3_bucket_acl" and
+  (rc.change.actions contains "create" or rc.change.actions contains "update") and
 
-  # Add app_user_data if provided
-  dynamic "part" {
-    for_each = var.app_user_data != null && var.app_user_data != "" ? [1] : []
-    content {
-      content_type = "text/x-shellscript"
-      content      = base64decode(var.app_user_data)
-    }
+  # Safely guard before looping
+  rc.change.after contains "access_control_policy" and
+  rc.change.after.access_control_policy contains "grant" and
+  rc.change.after.access_control_policy.grant is list and
+
+  any rc.change.after.access_control_policy.grant as grant {
+    grant contains "grantee" and
+    grant.grantee contains "uri" and
+    grant.grantee.uri is authenticated_users_uri and
+    (grant.permission in ["READ", "WRITE_ACP"])
   }
 }
-
-# Update locals to use the merged data
-
-
-  app_user_data     = var.app_user_data
-
-  # Use the merged data block output
-  user_data_base64  = data.template_cloudinit_config.merged.rendered
-}
-
