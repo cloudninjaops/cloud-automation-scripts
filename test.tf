fd9b244ce5271769a4fbe911583a5f61529ea55c
cloud ninja ops <cloudninjaops@gmail.com>
2025-10-15 14:36:49 -0400

    test hcl

--- test.tf
+++ test.tf
@@ -6,10 +6,10 @@ violating_acls = filter(tfplan.resource_changes or []) as address, rc {
   rc.type is "aws_s3_bucket_acl" and
   (rc.change.actions contains "create" or rc.change.actions contains "update") and
 
-  # Safely guard before looping
-  rc.change.after contains "access_control_policy" and
-  rc.change.after.access_control_policy contains "grant" and
-  rc.change.after.access_control_policy.grant is list and
+  # safe guard checks
+  (rc.change.after contains "access_control_policy") and
+  (rc.change.after.access_control_policy contains "grant") and
+  (type(rc.change.after.access_control_policy.grant) is list) and
 
   any rc.change.after.access_control_policy.grant as grant {
     grant contains "grantee" and
