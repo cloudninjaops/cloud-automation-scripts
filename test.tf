512c4a7c15c8953a30fe10fb17cbe413ecb6f255
cloud ninja ops <cloudninjaops@gmail.com>
2025-10-16 10:53:55 -0400

    test hcl

--- test.tf
+++ test.tf
@@ -1,27 +1,25 @@
 import "tfplan/v2" as tfplan
 
 violations = filter(tfplan.resource_changes or []) as _, rc {
+
   rc.type is "aws_s3_bucket_server_side_encryption_configuration" and
   (rc.change.actions contains "create" or rc.change.actions contains "update") and
 
-  # ---- exception: skip access-log buckets ----
-  (
-    not (
-      (rc.change.after contains "bucket") and
-      (type(rc.change.after.bucket) is string) and
-      (rc.change.after.bucket contains "s3-accesslog")
-    )
+  # --- exception: skip access log buckets ---
+  not (
+    (rc.change.after contains "bucket") and
+    (type(rc.change.after.bucket) is string) and
+    (rc.change.after.bucket contains "s3-accesslog")
   ) and
 
-  # ---- safe guards before reading fields ----
+  # --- safe guards ---
   (rc.change.after contains "rule") and
   (type(rc.change.after.rule) is list) and
   (length(rc.change.after.rule) > 0) and
-  (type(rc.change.after.rule[0]) is map) and
   (rc.change.after.rule[0] contains "apply_server_side_encryption_by_default") and
   (type(rc.change.after.rule[0].apply_server_side_encryption_by_default) is map) and
 
-  # ---- CMK must be used (NOT alias/aws/s3) ----
+  # --- fail if not CMK (aws:kms + key id required) ---
   not (
     (rc.change.after.rule[0].apply_server_side_encryption_by_default contains "sse_algorithm") and
     (rc.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm == "aws:kms") and
