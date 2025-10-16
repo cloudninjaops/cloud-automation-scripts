0fd37df5e2cfd6cca1d0c78b8a486af2ad38f958
cloud ninja ops <cloudninjaops@gmail.com>
2025-10-16 10:44:24 -0400

    test hcl

--- test.tf
+++ test.tf
@@ -1,20 +1,35 @@
 import "tfplan/v2" as tfplan
 
-authenticated_users_uri = "http://acs.amazonaws.com/groups/global/AuthenticatedUsers"
-
-violating_acls = filter(tfplan.resource_changes or []) as address, rc {
-  rc.type is "aws_s3_bucket_acl" and
+violations = filter(tfplan.resource_changes or []) as _, rc {
+  rc.type is "aws_s3_bucket_server_side_encryption_configuration" and
   (rc.change.actions contains "create" or rc.change.actions contains "update") and
 
-  # safe guard checks
-  (rc.change.after contains "access_control_policy") and
-  (rc.change.after.access_control_policy contains "grant") and
-  (type(rc.change.after.access_control_policy.grant) is list) and
+  # ---- exception: skip access-log buckets ----
+  (
+    not (
+      (rc.change.after contains "bucket") and
+      (type(rc.change.after.bucket) is string) and
+      (rc.change.after.bucket contains "s3-accesslog")
+    )
+  ) and
 
-  any rc.change.after.access_control_policy.grant as grant {
-    grant contains "grantee" and
-    grant.grantee contains "uri" and
-    grant.grantee.uri is authenticated_users_uri and
-    (grant.permission in ["READ", "WRITE_ACP"])
-  }
+  # ---- safe guards before reading fields ----
+  (rc.change.after contains "rule") and
+  (type(rc.change.after.rule) is list) and
+  (length(rc.change.after.rule) > 0) and
+  (type(rc.change.after.rule[0]) is map) and
+  (rc.change.after.rule[0] contains "apply_server_side_encryption_by_default") and
+  (type(rc.change.after.rule[0].apply_server_side_encryption_by_default) is map) and
+
+  # ---- CMK must be used (NOT alias/aws/s3) ----
+  not (
+    (rc.change.after.rule[0].apply_server_side_encryption_by_default contains "sse_algorithm") and
+    (rc.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm == "aws:kms") and
+    (rc.change.after.rule[0].apply_server_side_encryption_by_default contains "kms_master_key_id") and
+    (type(rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id) is string) and
+    (length(trim(rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id)) > 0) and
+    not (rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id contains "alias/aws/s3")
+  )
 }
+
+main = rule { length(violations) == 0 }
