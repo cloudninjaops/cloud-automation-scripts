ec7f80bfc04c09d0a16c2e31eb43c234b448d954
cloud ninja ops <cloudninjaops@gmail.com>
2025-10-16 11:19:08 -0400

    test hcl

--- test.tf
+++ test.tf
@@ -1,33 +1,36 @@
 import "tfplan/v2" as tfplan
 
-violations = filter(tfplan.resource_changes or []) as _, rc {
-
-  rc.type is "aws_s3_bucket_server_side_encryption_configuration" and
-  (rc.change.actions contains "create" or rc.change.actions contains "update") and
-
-  # --- exception: skip access log buckets ---
-  not (
-    (rc.change.after contains "bucket") and
-    (type(rc.change.after.bucket) is string) and
-    (rc.change.after.bucket contains "s3-accesslog")
-  ) and
-
-  # --- safe guards ---
-  (rc.change.after contains "rule") and
-  (type(rc.change.after.rule) is list) and
-  (length(rc.change.after.rule) > 0) and
-  (rc.change.after.rule[0] contains "apply_server_side_encryption_by_default") and
-  (type(rc.change.after.rule[0].apply_server_side_encryption_by_default) is map) and
-
-  # --- fail if not CMK (aws:kms + key id required) ---
-  not (
-    (rc.change.after.rule[0].apply_server_side_encryption_by_default contains "sse_algorithm") and
-    (rc.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm == "aws:kms") and
-    (rc.change.after.rule[0].apply_server_side_encryption_by_default contains "kms_master_key_id") and
-    (type(rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id) is string) and
-    (length(trim(rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id)) > 0) and
-    not (rc.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id contains "alias/aws/s3")
-  )
+# Consider only the dedicated SSE configuration resource on create/update.
+sse_cfgs = filter tfplan.resource_changes as _, r {
+  r.type is "aws_s3_bucket_server_side_encryption_configuration" and
+  (r.change.actions contains "create" or r.change.actions contains "update")
 }
 
-main = rule { length(violations) == 0 }
+# ---- Violations for access-log buckets: encryption REQUIRED (AES256 or any KMS) ----
+violations_accesslog = filter sse_cfgs as _, r {
+  # access-log bucket detection (safe: default bucket to "")
+  ((r.change.after.bucket else "") contains "s3-accesslog") and
+
+  # not encrypted as required
+  not (
+    ((r.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm else "") == "AES256") or
+    (
+      ((r.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm else "") == "aws:kms") and
+      (length(trim((r.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id else ""))) > 0)))
+}
+
+# ---- Violations for non-access-log buckets: must be KMS with a CMK (not alias/aws/s3) ----
+violations_cmk = filter sse_cfgs as _, r {
+  not (((r.change.after.bucket else "") contains "s3-accesslog")) and
+
+  # not using CMK
+  not (
+    ((r.change.after.rule[0].apply_server_side_encryption_by_default.sse_algorithm else "") == "aws:kms") and
+    (length(trim((r.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id else ""))) > 0) and
+    not (((r.change.after.rule[0].apply_server_side_encryption_by_default.kms_master_key_id else "") contains "alias/aws/s3")))
+}
+
+# Pass only if there are zero violations in both buckets classes.
+main = rule {
+  length(violations_accesslog) == 0 and length(violations_cmk) == 0
+}
