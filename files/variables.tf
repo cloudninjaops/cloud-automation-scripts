52e54e6b50d0ae77a8e0d49055e46430f00dc2e6
cloud ninja ops <cloudninjaops@gmail.com>
2026-03-03 12:16:32 -0500

    test

--- files/variables.tf
+++ files/variables.tf
@@ -15,3 +15,22 @@ variable "workspace_a_name" {
   description = "Name of Workspace A to read resources from"
   type        = string
 }
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "tag_costcenter" {
+  type = string
+}
+variable "tag_billingcode" {
+  type = string
+}
+variable "dry_run" {
+  type    = string
+  default = "true"   # safe default — dry-run unless explicitly false
+}
+variable "trigger_tag_update" {
+  type    = string
+  default = "v1"     # bump this value to trigger a new tagging run
+}
